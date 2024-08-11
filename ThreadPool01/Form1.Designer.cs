df1c3128c12214dcd24b3f19a83b049246cf7fff
knagasa <161251617+knagasa@users.noreply.github.com>
2024-08-12 06:05:09 +0900

    Add files via upload

---
+++ ThreadPool01/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace ThreadPool01
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.buttonDo = new System.Windows.Forms.Button();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(12, 12);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.textBox1.Size = new System.Drawing.Size(480, 426);
+			this.textBox1.TabIndex = 1;
+			// 
+			// buttonDo
+			// 
+			this.buttonDo.Location = new System.Drawing.Point(498, 12);
+			this.buttonDo.Name = "buttonDo";
+			this.buttonDo.Size = new System.Drawing.Size(235, 30);
+			this.buttonDo.TabIndex = 2;
+			this.buttonDo.Text = "ちょっと時間がかかる計算";
+			this.buttonDo.UseVisualStyleBackColor = true;
+			this.buttonDo.Click += new System.EventHandler(this.buttonDo_Click);
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(647, 56);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(86, 23);
+			this.comboBox1.TabIndex = 2;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(498, 59);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(129, 15);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "使用するCPUコア数:";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(498, 107);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(75, 15);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "CPUコア数:";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.buttonDo);
+			this.Controls.Add(this.textBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button buttonDo;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+	}
+}
+
---
+++ ThreadPool01/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace ThreadPool01
+{
+	public partial class Form1 : Form
+	{
+		const string LogFile = "log.txt";
+
+		int Cores = 1;
+		PerformanceCounter CpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+		Process ProcessCpuUsage = null;
+
+		public Form1()
+		{
+			InitializeComponent();
+
+			string[] args = Environment.GetCommandLineArgs();
+			if (args.Length == 1)
+			{
+				int workerMin;
+				int ioMin;
+
+				ThreadPool.GetMinThreads(out workerMin, out ioMin);
+
+				Cores = workerMin;
+
+				label2.Text = $"CPUコア数: {Cores}";
+
+				for (int i = 1; i <= Cores; i++)
+				{
+					comboBox1.Items.Add(i);
+				}
+				comboBox1.SelectedIndex = Cores - 1;
+
+				ProcessStartInfo psi = new ProcessStartInfo();
+				psi.FileName = args[0];
+				psi.Arguments = "abc";
+				psi.WindowStyle = ProcessWindowStyle.Minimized;
+				ProcessCpuUsage = Process.Start(psi);
+			}
+			else
+			{
+				this.Text = "CpuUsage";
+
+				WriteCpuUsage();
+			}
+
+
+		}
+
+		private void WriteCpuUsage()
+		{
+			if (File.Exists(LogFile))
+			{
+				File.Delete(LogFile);
+			}
+
+			System.Timers.Timer timer = new System.Timers.Timer();
+			timer.Elapsed += new System.Timers.ElapsedEventHandler(MyTimer);
+			timer.Interval = 500;
+			timer.Start();
+		}
+
+		private void MyTimer(object sender, ElapsedEventArgs e)
+		{
+			DateTime dt = DateTime.Now;
+
+			float cpu = CpuCounter.NextValue();
+
+			string msg = $"{dt.ToString("yyyy/MM/dd HH:mm:ss.fff")}, {cpu}" + Environment.NewLine;
+
+			File.AppendAllText(LogFile, msg);
+		}
+
+		private async void buttonDo_Click(object sender, EventArgs e)
+		{
+			buttonDo.Enabled = false;
+			comboBox1.Enabled = false;
+
+			SetCoreNum(comboBox1.SelectedIndex + 1);
+
+			await F1();
+
+			buttonDo.Enabled = true;
+			comboBox1.Enabled = true;
+		}
+
+		private void SetCoreNum(int n)
+		{
+			int workerMin;
+			int ioMin;
+			int workerMax;
+			int ioMax;
+
+			ThreadPool.GetMinThreads(out workerMin, out ioMin);
+			ThreadPool.GetMaxThreads(out workerMax, out ioMax);
+			while (true)
+			{
+				ThreadPool.SetMinThreads(n, ioMin);
+				ThreadPool.SetMaxThreads(n, ioMin);
+
+				ThreadPool.GetMinThreads(out workerMin, out ioMin);
+				ThreadPool.GetMaxThreads(out workerMax, out ioMax);
+				if (workerMin == n && workerMax == n)
+				{
+					break;
+				}
+			}
+
+			textBox1.AppendText($"使用するコア数を {n} に設定" + Environment.NewLine);
+		}
+
+		private async Task F1()
+		{
+			int nThreads = Cores;
+			int max = 50_000_000;
+
+			List<Task> tasks = new List<Task>();
+
+			DateTime dtStart = DateTime.Now;
+
+			for (int i = 0; i < nThreads; i++)
+			{
+				Task task = Task.Run(() =>
+				{
+					int idx = i;
+
+					DateTime dt = DateTime.Now;
+					string msg = $"{dt.ToString("HH:mm:ss.fff")} thread {Thread.CurrentThread.ManagedThreadId} start";
+					this.Invoke(new Action(() =>
+					{
+						textBox1.AppendText(msg + Environment.NewLine);
+					}));
+
+					for (int j = idx; j < max; j += nThreads)
+					{
+						int x = (j + 1) * 2 + 1;
+						IsPrime(x);
+					}
+
+					dt = DateTime.Now;
+					msg = $"{dt.ToString("HH:m:ss.fff")} thread {Thread.CurrentThread.ManagedThreadId} end";
+					this.Invoke(new Action(() =>
+					{
+						textBox1.AppendText(msg + Environment.NewLine);
+					}));
+
+				});
+				tasks.Add(task);
+			}
+
+			await Task.WhenAll(tasks);
+
+			DateTime dtEnd = DateTime.Now;
+			TimeSpan span = dtEnd - dtStart;
+
+			textBox1.AppendText($"実行時間: {span.ToString("hh':'mm':'ss'.'fff")}" + Environment.NewLine);
+
+			float cpuUsage = GetCpuUsage(dtStart, dtEnd);
+			textBox1.AppendText($"CPU使用率: {cpuUsage} %" + Environment.NewLine);
+		}
+
+		private float GetCpuUsage(DateTime dtStart, DateTime dtEnd)
+		{
+			float sum = 0;
+			int count = 0;
+
+			string[] lines = File.ReadAllLines(LogFile);
+
+			foreach (string line in lines)
+			{
+				string[] strs = line.Split(',');
+
+				DateTime dt = DateTime.Parse(strs[0]);
+
+				if (dtStart < dt &&  dt < dtEnd)
+				{
+					sum += float.Parse(strs[1]);
+					count++;
+
+					// textBox1.AppendText(strs[1] + Environment.NewLine);
+				}
+			}
+
+			return sum / count;
+		}
+
+		private bool IsPrime(int x)
+		{
+			if (x < 2)
+			{
+				return false;
+			}
+			if (x == 2)
+			{
+				return true;
+			}
+			if (x % 2 == 0)
+			{
+				return false;
+			}
+
+			double sqrt = Math.Sqrt(x);
+			for (int i = 3; i < sqrt; i += 2)
+			{
+				if (x % i == 0)
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			if (ProcessCpuUsage != null)
+			{
+				try
+				{
+					ProcessCpuUsage.CloseMainWindow();
+					ProcessCpuUsage.Close();
+				}
+				catch
+				{
+				}
+			}
+		}
+	}
+}
